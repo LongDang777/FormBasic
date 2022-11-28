9f8480fc450cd2b6b6071bc5f5db813bbc2fe5de
Long <dangtienlong2020@gmail.com>
2022-11-28 22:06:48 +0700

    fix input form

--- main.js
+++ main.js
@@ -23,13 +23,13 @@ const validateInput = (id, seiral, message) => {
     failureIcon[seiral].style.opacity = "0";
     successIcon[seiral].style.opacity = "1";
   }
-  if (
-    errorMes[0].innerHTML === "" &&
-    errorMes[1].innerHTML === "" &&
-    errorMes[2].innerHTML === ""
-  ) {
-    alert("Congratulations, you have successfully registered an account");
-  }
+  // if (
+  //   errorMes[0].innerHTML === "" &&
+  //   errorMes[1].innerHTML === "" &&
+  //   errorMes[2].innerHTML === ""
+  // ) {
+  //   alert("Congratulations, you have successfully registered an account");
+  // }
 };
 //  ! tìm lại form trn git validation
 
